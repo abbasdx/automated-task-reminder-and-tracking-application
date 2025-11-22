5cb4022bb60e3b2097740bd231200ed280217dbc
Abbas Ansari <iamabbas.self@gmail.com>
2025-11-22 18:55:35 +0530

    Initial commit

---
+++ src/main/java/com/abbasansari/taskreminder/AutomatedTaskReminderAndTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.abbasansari.taskreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTaskReminderAndTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTaskReminderAndTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abbasansari/taskreminder/AutomatedTaskReminderAndTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbasansari.taskreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTaskReminderAndTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
