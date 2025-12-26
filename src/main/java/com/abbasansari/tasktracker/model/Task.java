b5be2e194e81605bf19fa485b28feb00b4be9b9d
Abbas Ansari <iamabbas.self@gmail.com>
2025-12-26 15:34:13 +0530

    update task entity

--- src/main/java/com/abbasansari/tasktracker/model/Task.java
+++ src/main/java/com/abbasansari/tasktracker/model/Task.java
@@ -1,18 +1,27 @@
 package com.abbasansari.tasktracker.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tasks")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String description;
+
+    @Column(name = "due_date")
     private LocalDateTime dueDate;
     private boolean completed;
 
