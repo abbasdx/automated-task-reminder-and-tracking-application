212a2e75ae9ebc8ba34420bc437aab09134cafb1
Abbas Ansari <iamabbas.self@gmail.com>
2025-12-28 13:39:59 +0530

    add task controller

---
+++ src/main/java/com/abbasansari/tasktracker/controller/TaskController.java
@@ -0,0 +1,32 @@
+package com.abbasansari.tasktracker.controller;
+
+import com.abbasansari.tasktracker.dto.TaskRequestDto;
+import com.abbasansari.tasktracker.model.Task;
+import com.abbasansari.tasktracker.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestBody TaskRequestDto dto) {
+        taskService.createTask(dto);
+        return "Task added successfully";
+    }
+
+    @GetMapping("/list")
+    public List<Task> list() {
+        return taskService.getAllTasks();
+    }
+
+}
+
