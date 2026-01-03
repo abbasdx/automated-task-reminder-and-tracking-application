1c3f399de7fce76164d0fc6992fb8b0a998dafa6
Abbas Ansari <iamabbas.self@gmail.com>
2026-01-03 22:10:53 +0530

    update task request dto

--- src/main/java/com/abbasansari/tasktracker/dto/TaskRequestDto.java
+++ src/main/java/com/abbasansari/tasktracker/dto/TaskRequestDto.java
@@ -1,5 +1,7 @@
 package com.abbasansari.tasktracker.dto;
 
+import com.abbasansari.tasktracker.model.Category;
+import com.abbasansari.tasktracker.model.Priority;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,8 @@ public class TaskRequestDto {
     private String title;
     private String description;
     private LocalDateTime dueDate;
+    private Priority priority;
+    private Category category;
 
 }
 
