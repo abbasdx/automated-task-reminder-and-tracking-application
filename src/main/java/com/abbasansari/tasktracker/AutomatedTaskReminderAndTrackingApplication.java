e464dc992b9c2c8f0780ce48e9c29e54ce9941c5
Abbas Ansari <iamabbas.self@gmail.com>
2026-01-05 19:42:25 +0530

    update package name

--- src/main/java/com/abbasansari/tasktracker/AutomatedTaskReminderAndTrackingApplication.java
+++ src/main/java/com/abbasansari/tasktracker/AutomatedTaskReminderAndTrackingApplication.java
@@ -1,4 +1,4 @@
-package com.abbasansari.taskreminder;
+package com.abbasansari.tasktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
