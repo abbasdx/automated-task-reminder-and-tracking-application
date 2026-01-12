b5b83f0cfdcf5e8e15e243e7b575a826aeab720d
Abbas Ansari <iamabbas.self@gmail.com>
2026-01-12 22:20:16 +0530

    update csv util

--- src/main/java/com/abbasansari/tasktracker/util/CsvUtil.java
+++ src/main/java/com/abbasansari/tasktracker/util/CsvUtil.java
@@ -7,11 +7,12 @@ import java.util.List;
 public class CsvUtil {
 
     public static void writeTasks(PrintWriter writer, List<Task> tasks) {
-        writer.println("ID,Title,Completed,Priority,Category");
+        writer.println("ID,Title,Description,Completed,Priority,Category");
         for (Task task : tasks) {
             writer.println(
                     task.getId() + "," +
                             task.getTitle().replace(",", " ") + "," +
+                            task.getDescription().replace(",", " ") + "," +
                             task.isCompleted()+","+
                             task.getPriority()+","+
                             task.getCategory()
