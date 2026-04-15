1ab2c8ddf3e723d9a18581ca43a30bd513f0a6d9
Abbas Ansari <iamabbas.self@gmail.com>
2026-04-15 21:24:26 +0530

    update scheduler service

--- src/main/java/com/abbasansari/tasktracker/service/SchedulerService.java
+++ src/main/java/com/abbasansari/tasktracker/service/SchedulerService.java
@@ -3,7 +3,6 @@ package com.abbasansari.tasktracker.service;
 import com.abbasansari.tasktracker.model.Task;
 import com.abbasansari.tasktracker.scheduler.ReminderExecutor;
 import org.springframework.stereotype.Service;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
@@ -18,19 +17,57 @@ public class SchedulerService {
     }
 
     public void scheduleReminder(Task task) {
-        if (task.getDueDate() == null) return;
 
-        long delay = Duration.between(LocalDateTime.now(), task.getDueDate()).toMillis();
-        if (delay <= 0) return;
+        if (!task.getUser().isEmailVerified()
+                || task.getDueDate() == null) {
+            return;
+        }
 
-        String userEmail = task.getUser().getEmail();
-        String userName  = task.getUser().getName();
-        String title     = task.getTitle();
-        String desc      = task.getDescription();
         LocalDateTime dueDate = task.getDueDate();
 
+        scheduleSingleReminder(
+                dueDate.minusHours(1),
+                task,
+                "1 Hour Remaining"
+        );
+
+        scheduleSingleReminder(
+                dueDate.minusMinutes(10),
+                task,
+                "10 Minutes Remaining"
+        );
+
+        scheduleSingleReminder(
+                dueDate,
+                task,
+                "Task Deadline Reached"
+        );
+    }
+
+    private void scheduleSingleReminder(
+            LocalDateTime reminderTime,
+            Task task,
+            String reminderType
+    ) {
+
+        long delay = Duration.between(
+                LocalDateTime.now(),
+                reminderTime
+        ).toMillis();
+
+        if (delay <= 0) {
+            return;
+        }
+
         ReminderExecutor.getExecutor().schedule(
-                () -> mailService.sendScheduledReminder(userEmail, userName, title, desc, dueDate),
+                () -> mailService.sendReminderEmail(
+                        task.getUser().getEmail(),
+                        task.getUser().getName(),
+                        task.getTitle(),
+                        task.getDescription(),
+                        task.getDueDate(),
+                        reminderType
+                ),
                 delay,
                 TimeUnit.MILLISECONDS
         );
